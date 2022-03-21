13bfcef29a5fa0899c7d3d6b8caa9a317c6518ef
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-03-21 18:14:58 -0300

    Código tp1_1.cpp v2

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,6 +3,16 @@
 int main()
 {
     printf("Hola, mundo\n");
+
+    int vble = 12, *punt;
+    punt = &vble;
+
+    printf("Contenido del puntero: %d\n", *punt);
+    printf("Direccion de memoria almacenada en el puntero: %d\n", punt);
+    printf("Direccion de memoria de la variable: %d\n", &vble);
+    printf("Direccion de memoria del puntero: %d\n", &punt);
+    printf("Tamano de la variable: %d\n", sizeof(&vble));
+
     getchar();
     return 0;
 }
